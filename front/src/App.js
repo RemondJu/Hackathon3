dd159959768e3e8549d9ed2622976b007aaf0ae7
Thomas Gonzalez <thomasgonzalez.pro@gmail.com>
2018-12-20 16:45:21 +0100

    navbar

--- front/src/App.js
+++ front/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import HomePage from './components/HomePage';
 import { Switch, Route } from 'react-router-dom';
 import MyHome from './components/MyHome';
+import Navigation from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        EC'HOME
+        <Navigation />
         <Switch >
           <Route exact path="/" component={HomePage} />
           <Route path="/myHome" component={MyHome} />
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,11 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+  import './Navbar.css';
 
-const Navbar = () => {
-  return ( 
-    <div>
-      
-    </div>
-   );
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="Nav" >
+        <Navbar color="light" light expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem className="nav-items" >
+                <NavLink href="/myhome/"> <i className="fas fa-home"></i> </NavLink>
+              </NavItem>
+              <NavItem className="nav-items" >
+                <NavLink href="/favoris/"> <i className="fas fa-star"></i> </NavLink>
+              </NavItem>
+              <NavItem className="nav-items" >
+                <NavLink href="/homepage/"> <img className="logo" src="../images/logo.png" alt="logoNavbar" height="50" width="100" /> </NavLink>
+              </NavItem>
+              <NavItem className="nav-items" >
+                <NavLink href="/trophees/"> <i className="fas fa-trophy"></i> </NavLink>
+              </NavItem>
+              <NavItem className="nav-items" >
+                <NavLink href="/moncompte/"> <i className="items fas fa-user-circle"></i> </NavLink>
+              </NavItem>  
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
- 
-export default Navbar;
