aa11d900af7d00309b90ee3a95a9edab0ac31cac
RemondJu <remond.neiluj@gmail.com>
2018-12-20 21:05:57 +0100

    My rooms dynamic version

--- front/src/containers/MyRooms.jsx
+++ front/src/containers/MyRooms.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Col, Row } from 'reactstrap';
-import Object from '../components/Object';
+import ObjectInRoom from '../components/ObjectInRoom';
 import { connect } from 'react-redux';
 import '../components/MyRooms.css'  
 import { roomClick } from '../actions'
 import { bindActionCreators } from 'redux';
 
-const MyRooms = (props) => (
-  <div className="MyRooms">
-    <Row className="roomsRow">
-      <Col sm="2" className="roomsSide">
-        {props.roomList.map((room, index) => {
-          return (
-            <button key={room.id} type="button" className="room" onClick={() => props.roomClick(index)} name={room.type}>
-              {room.name}
-            </button>
-          );
-        })}
-      </Col>
-      <Col className="text-center mt-5">
-        <h2 className="roomName">Salon</h2>
-        <div className="objects">
-          {props.roomObjects.map(object => {
-            return(
-              <Object
-                key={object.id}
-                name={object.name}
-                type={object.type}
-                consumption={object.consumption}
-              />
-            )
-          })}
-        </div>        
-      </Col>      
-    </Row>
-  </div>
-)
 
+
+class MyRooms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      roomName: this.props.roomList[0].name
+     }
+     this.handleRoomClick = this.handleRoomClick.bind(this);
+  }
+
+  handleRoomClick(idx){
+    this.setState({
+      roomName: this.props.roomList[idx].name,
+    });
+    this.props.roomClick(idx);  
+  }
+
+  render() { 
+    return ( 
+      <div className="MyRooms">
+        <Row className="roomsRow">
+          <Col sm="2" className="roomsSide">
+            {this.props.roomList.map((room, index) => {
+              return (
+                <button key={room.id} type="button" className="room" onClick={() => this.handleRoomClick(index)} name={room.type}>
+                  {room.name}
+                </button>
+              );
+            })}
+          </Col>
+          <Col className="text-center mt-5 mr-5">
+            <h2 className="roomName">{this.state.roomName}</h2>
+            <div className="objects">
+              {this.props.roomObjects.map(object => {
+                return(
+                  <ObjectInRoom
+                    key={object.id}
+                    name={object.name}
+                    type={object.type}
+                    consumption={object.consumption}
+                    description={object.description}
+                  />
+                )
+              })}
+            </div>        
+          </Col>      
+        </Row>
+      </div>
+     );
+  }
+}
+ 
 function mstp(state) {
   return {
     roomList: state.roomList,
