dbfc1401c76cc9946b8ddea1d7e2b3960186837e
MoralesKevin <moraleskevin1502@gmail.com>
2018-12-21 10:40:15 +0100

    habillage page objets

--- front/src/components/ObjectInRoom.jsx
+++ front/src/components/ObjectInRoom.jsx
@@ -5,16 +5,16 @@ import './Object.css';
 class ObjectInRoom extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       objectDivClass: 'hidden',
       switch: true,
       switchimg: "./images/switchon.png",
-     }
-     this.toggleInfos = this.toggleInfos.bind(this);
+    }
+    this.toggleInfos = this.toggleInfos.bind(this);
   }
 
-  toggleInfos(){
-    if(this.state.objectDivClass === 'hidden'){
+  toggleInfos() {
+    if (this.state.objectDivClass === 'hidden') {
       this.setState({
         objectDivClass: 'show',
       })
@@ -26,33 +26,37 @@ class ObjectInRoom extends Component {
   }
 
   switchOff() {
-    if (this.state.switch === true ){
-    this.setState({
-      switch: false,
-      switchimg: "./images/switchoff.png",
-    })
-  } else {
-    this.setState({
-      switch: true,
-      switchimg: "./images/switchon.png",
+    if (this.state.switch === true) {
+      this.setState({
+        switch: false,
+        switchimg: "./images/switchoff.png",
       })
-  }
+    } else {
+      this.setState({
+        switch: true,
+        switchimg: "./images/switchon.png",
+      })
+    }
   }
 
-  render() { 
-    return ( 
-      <div className="Object">
+  render() {
+    return (
+      <div className="Object" >
         <Row className="objectRow mr-1 ml-0">
-          <img src={this.props.img} alt="img" onClick={() => this.toggleInfos()} />
+          <img src={this.props.img} alt="img" />
           <p><strong>Objet : </strong>{this.props.name}</p>
           <p><strong>Type : </strong>{this.props.type}</p>
           <p><strong>Consommation : </strong> {this.props.consumption} W</p>
           <button className="switch" onClick={() => this.switchOff()}>  <img src={this.state.switchimg} alt="Login" width="30%" height="30%" /> </button>
         </Row>
+        <Row>
+          <button className="moreDetail" onClick={() => this.toggleInfos()}><i class="fas fa-angle-down"></i></button>
+        </Row>
+
         <div className={this.state.objectDivClass}>{this.props.description}</div>
       </div>
-     );
+    );
   }
 }
- 
+
 export default ObjectInRoom;
