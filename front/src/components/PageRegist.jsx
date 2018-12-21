b4429c1399dc72aed6009a09d81a8c69fc82560c
Alissone Neyret <alissone.neyret@gmail.com>
2018-12-21 10:58:21 +0100

    ajustements

--- front/src/components/PageRegist.jsx
+++ front/src/components/PageRegist.jsx
@@ -7,7 +7,8 @@ export default class Example extends React.Component {
   render() {
     return (
       <Container fluid>
-      <NavLink to="/"><Button className="page-register-button">RETOUR</Button></NavLink>
+      <NavLink to="/"><Button className="homepage-button-first2" >RETOUR</Button></NavLink>
+      <h3 className="h3-pageRegist">Je crée mon compte</h3>
         <Row className="page-register-wrap">
           <Col lg={4} md={6} sm={12}>
             <Form>
@@ -27,7 +28,7 @@ export default class Example extends React.Component {
                 <Label for="examplePassword">Confirmer mot de passe</Label>
                 <Input type="passwordConfirm" name="passwordconfirm" />
               </FormGroup>
-              <NavLink to="/accueil"><Button color="primary">Submit</Button></NavLink>
+              <NavLink to="/accueil"><Button className="homepage-button-first" color="primary">Créer</Button></NavLink>
             </Form>
           </Col>
         </Row>
